d1b9cd18c06ba5e4306b9ea17ff411cd3968355a
thummalaharika17 <thummalaharika17@gmail.com>
2025-04-13 19:45:10 +0530

    added app password

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -138,9 +138,13 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CORS_ALLOW_ALL_ORIGINS = True
+ALLOWED_HOSTS = ['*']
+
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'thummalaharika17@gmail.com'
+EMAIL_HOST_USER = 'otpauthentication25@gmail.com'
+EMAIL_HOST_PASSWORD = 'evlb emqv krmg lmpz'
+
